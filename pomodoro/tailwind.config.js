3e47e8a83b7259ee9a2cc270bcb7f2a94d1ae8dd
Mgalinri <galindom@jbu.edu>
2025-06-20 05:51:47 -0500

    The timer is missing other controls and the sound

---
+++ pomodoro/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        d7:['var(--font-d7)'],
+      }
+    },
+  },
+  plugins: [],
+}
+
